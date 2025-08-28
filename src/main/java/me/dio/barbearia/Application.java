bba500cc9057b23cd3cb4346ce857d15fbda9e73
Felipe Boaventura <correa.felip3@gmail.com>
2025-08-28 16:04:21 -0300

    Initial commit

---
+++ src/main/java/me/dio/barbearia/Application.java
@@ -0,0 +1,13 @@
+package me.dio.barbearia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/me/dio/barbearia/BarbeariaApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.barbearia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarbeariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
